c438cff6588c58ce7fb6267e1199cec40a946032
rejas <github@veeck.de>
2021-09-09 15:02:55 +0200

    Add github action for deployment

--- src/server.js
+++ src/server.js
@@ -6,8 +6,11 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
+const url = dev ? '/' : '/cookblog';
+
 polka() // You can also use Express
 	.use(
+		url,
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
